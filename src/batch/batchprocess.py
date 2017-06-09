1baca45354468f13ff99992fd2d734477eaab981
wanghan1110 <wanghan1110@gmail.com>
2017-06-09 00:03:51 -0700

    First commit

---
+++ src/batch/batchprocess.py
@@ -0,0 +1,45 @@
+import time
+from pyspark.sql.types import *
+from pyspark.sql.functions import udf
+
+# convert RDD obj to DataFrame
+def getDataFrame(rddObj):
+    rdd = rddObj.map(lambda x : x.split(','))
+    header = rdd.first()
+    df = rdd.filter(lambda row : row != header).toDF(header)
+    return df
+
+# join dataframes
+def joinTable(df1,df2):
+    joinDF = df1.join(df2,df1.bs==df2.bs,'inner').\
+    select(df1.bs,df1.users,df2.lon,df2.lat,df1.time_hour)
+    return joinDF
+
+# convert df column to numbers
+def convertNum(df):
+    df_num = df.select(df.users.cast("integer"),\
+        df.lon.cast("float"),df.lat.cast("float"),\
+        df.time_hour.cast("integer"))
+    return df_num
+
+# convert epoch time to GMT-time. return GMT hour
+def getGMT(epoch):
+    hour = time.gmtime(epoch).tm_hour
+    return float(hour)
+
+if __name__ == '__main__':
+    cellLink = 's3a://cellulartest/cellular_traffic.csv'
+    topoLink = 's3a://cellulartest/topology.csv'
+    cellRDD = sc.textFile(cellLink)
+    topoRDD = sc.textFile(topoLink)
+
+    cellDF = getDataFrame(cellRDD)
+    topoDF = getDataFrame(topoRDD)
+
+    joinDF = joinTable(cellDF,topoDF)
+    joinDF_num = convertNum(joinDF)
+
+    # define getGMT as UDF
+    udf_gmt = udf(getGMT,FloatType())
+    joinDF_num = joinDF_num.withColumn("hour",udf_gmt(joinDF_num['time_hour']))
+

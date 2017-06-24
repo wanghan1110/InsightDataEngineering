a0f966324d40e5949365ec9a5052f967795e805f
wanghan1110 <wanghan1110@gmail.com>
2017-06-24 15:54:56 -0700

    Delete localstartmsg.sh

--- src/streaming/localstartmsg.sh
+++
@@ -1,7 +0,0 @@
-bash spawn_kafka_streams.sh 34.211.247.230 120 k1
-# bash spawn_kafka_streams.sh 52.10.138.212 120 k2
-# bash spawn_kafka_streams.sh 35.167.89.94 120 k3
-# bash spawn_kafka_streams.sh 35.160.69.184 120 k4
-
-# kill the process
-# tmux kill-session -t k1
